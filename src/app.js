6273a153fd6d43017cf0937e16941814347e68c2
Victoria Offoma <offoma.victoria@gmail.com>
2020-03-31 20:34:30 +0100

    chore: add packages

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import indexRouter from './controllers/indexRouter';
 
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.use('/api/v0', indexRouter);
 
