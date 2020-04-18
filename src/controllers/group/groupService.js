9af90e560eb92ca97477997b416d59216409e24c
Victoria Offoma <offoma.victoria@gmail.com>
2020-04-18 09:37:21 +0100

    Fix: return latest member details on joining group

--- src/controllers/group/groupService.js
+++ src/controllers/group/groupService.js
@@ -19,8 +19,20 @@ const addUserToGroup = async (joinRequest) => {
   // implement check to see if the group is a private group or not
   try {
     const { groupId, user } = joinRequest;
-    const newMember = await Group.findOneAndUpdate({ _id: groupId, 'members.email': { $ne: user.email } }, { $push: { members: user } });
-    return newMember;
+
+    const updatedGroup = await Group.findOneAndUpdate({ _id: groupId, 'members.email': { $ne: user.email } }, { $push: { members: user } });
+
+    if (updatedGroup) {
+      const { name, description, savingsAmount } = updatedGroup;
+      const groupInfo = {
+        name,
+        description,
+        savingsAmount,
+        latestMember: { email: user.email, name: user.name },
+      };
+      return groupInfo;
+    }
+    return 'Hello, you are already a member of this group.';
   } catch (error) {
     throw createError(400, error);
   }
