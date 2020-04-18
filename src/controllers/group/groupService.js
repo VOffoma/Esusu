66da50cba3867929fff1863a3e98129468df5e59
Victoria Offoma <offoma.victoria@gmail.com>
2020-04-18 08:35:32 +0100

    feature: make admin the first user of a group

--- src/controllers/group/groupService.js
+++ src/controllers/group/groupService.js
@@ -2,8 +2,11 @@ import createError from 'http-errors';
 import Group from './models/Group';
 
 const createGroup = async (groupDetails) => {
-  const group = await (new Group(groupDetails)).save();
-  return group;
+  const { group, user } = groupDetails;
+  group.admin = user;
+  group.members = [user];
+  const newGroup = await (new Group(group)).save();
+  return newGroup;
 };
 
 const getGroups = async () => {
